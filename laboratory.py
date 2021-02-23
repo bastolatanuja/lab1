4850acde37c6df896e9a8dd941821cb37a2c60c5
bastolatanuja <tanujabastola143@gmail.com>
2021-02-23 11:19:38 +0545

    Initial commit

---
+++ laboratory.py
@@ -0,0 +1,6 @@
+#write a program that takes three numbers and print their sum.every number is given on a separate line.
+val1=int(input('enter the first number: '))
+val2=int(input("enter the second number: "))
+val3=int(input("enter the third number: "))
+add=val1+val2+val3
+print('sum of three number is',add)
