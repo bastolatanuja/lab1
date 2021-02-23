e447ce0f8d44abcc6e5095beae34d1bc78c0d45e
bastolatanuja <tanujabastola143@gmail.com>
2021-02-23 12:22:43 +0545

    NO OF STUDENTS AND APPLES

--- question 3.py
+++ question 3.py
@@ -1,5 +1,11 @@
 # N students take K apples and distribute them among each other evenly.
-# The remaining ( undivisible0 part remains in the basket.
+# The remaining ( undivisible) part remains in the basket.
 # How many apples will each single student get?
 # How many apples will remain in the basket?
 # The program reads the numbers N and K. It should print the two answers for the question above.
+N=int(input("emter the number of students: "))
+K=int(input("enter the number of apples: "))
+D=K/N
+R=K//N
+print(f"the number of apple got by student is{D}")
+print(f"the apples left in the basket is{R}")
