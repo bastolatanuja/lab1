9235288a9bb29a0bc740530747aee40503361a0a
bastolatanuja <tanujabastola143@gmail.com>
2021-03-09 11:25:12 +0545

    bmi calculation

--- lab exercise/question number 6.py
+++ lab exercise/question number 6.py
@@ -2,7 +2,11 @@
 #when there is a final answer have python print it to the screen.
 #a person's body mass index (BMI) is defined as:
 #BMI=mass in kg/ (height in m)2
-mass=float(input("enter the mass in kg: "))                     #mass in kg
-height=float(input("enter the height in m: "))                  #height in m
+
+mass=float(input("enter the mass in kg: "))
+#mass in kg
+height=float(input("enter the height in m: "))
+#height in m
 bmi= mass/(height*height)
+
 print(f"the bmi is{bmi}")
